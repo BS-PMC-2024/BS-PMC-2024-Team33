dba89153e37e8ca00c21209864ccc60d5c7f7496
Yones Nimer <yones.nmer@gmail.com>
2024-07-13 18:27:09 +0300

    feat: Add filter and view coding problems - Added `ProblemFilterForm` to allow filtering of coding problems by language and problem name. - Updated `CodeProblemForm` to include a dropdown for language selection. - Enhanced the `ViewProblemsForStudent` view to handle filtering by language and problem name. - Updated `addcodepage.html` to include the language field in the form. - Created `Showing_Problems.html` to display the filtered list of problems and the filter form. related issues - BSPMS2433-33, BSPMS2433-56

--- CodingBuddy/CBapp/forms.py
+++ CodingBuddy/CBapp/forms.py
@@ -4,4 +4,7 @@ from .models import CodeProblem
 class CodeProblemForm(forms.ModelForm):
     class Meta:
         model = CodeProblem
-        fields = ['problem', 'description','solution']
+        fields = ['problem', 'description','solution','language']
+
+class ProblemFilterForm(forms.Form):
+    language = forms.CharField(max_length=50, required=False)
--- CodingBuddy/CBapp/models.py
+++ CodingBuddy/CBapp/models.py
@@ -3,7 +3,10 @@ from django.db import models
 class CodeProblem(models.Model):
     problem = models.CharField(max_length=100)
     description = models.CharField(max_length=100, default="")
-    solution = models.CharField(max_length=100, default="")
+    solution = models.CharField(max_length=1000, default="")
+    status = models.CharField(max_length=20, default="not accepted")
+    language = models.CharField(max_length=50)
+
 
     def __str__(self):
         return self.problem
